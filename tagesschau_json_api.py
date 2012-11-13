f6739db30359b84cbec3fa7f05a3d23008235a5a
Henning Saul <henning.saul@gmx.net>
2012-11-12 21:10:44 -0500

    some docstrings

--- tagesschau_json_api.py
+++ tagesschau_json_api.py
@@ -9,7 +9,18 @@ import logging
 logger = logging.getLogger(__name__)
 
 class VideoContent(object):
+    """Represents a single video or broadcast.
+
+    Attributes:
+        title: A String with the video's title
+        timestamp: A datetime when this video was broadcast
+        videourls: A dict mapping video variant Strings to their URL Strings
+        imageurls: A dict mapping image variants Strings to their URL Strings
+        duration: An integer representing the length of the video in seconds
+    """    
+    
     def __init__(self, title, timestamp, videourls, imageurls=None, duration=None):
+        """Inits VideoContent with the given values."""
         self.title = title
         # datetime
         self.timestamp = timestamp
@@ -20,28 +31,58 @@ class VideoContent(object):
         # image variant names mapped to urls
         self.imageurls = imageurls
     
-    def video_url(self):
-        # TODO: broadcasts also have "adaptivestreaming"
-        videourl = self.videourls["h264l"]
+    def video_url(self, quality):
+        """Returns the video URL String for the given quality.
+        
+        Falls back to lower qualities if no corresponding video is found.
+        
+        Args:
+            quality: One of 'S', 'M' or 'L'
+
+        Returns:
+            A URL String for the given quality or None if no URL could be found.
+
+        Raises:
+            ValueError: If the given quality is invalid
+        """
+        if (not quality in ['S', 'M', 'L']):
+            raise ValueError("quality must be one of 'S', 'M', 'L'")
+        # TODO: use adaptivestreaming if available?
+        videourl = _get(self.videourls, "adaptivestreaming")
+        if(quality == 'L' or not videourl):
+            videourl = _get(self.videourls, "h264l")
+        if(quality == 'M' or not videourl):    
+            videourl = _get(self.videourls, "h264m")
+        if(quality == 'S' or not videourl):    
+            videourl = _get(self.videourls, "h264s")
         return videourl
 
     def image_url(self):
-        try:
-            imageurl = self.imageurls["mittel16x9"]
-        # fallback for Wetter
-        except KeyError:
-            imageurl = self.imageurls["grossgalerie16x9"]
+        """Returns the URL String of the image for this video."""
+        imageurl = _get(self.imageurls, "mittel16x9")
+        if(not imageurl):
+            # fallback for Wetter
+            imageurl = _get(self.imageurls, "grossgalerie16x9")
         return imageurl
         
-    # String representation for testing/development
     def __str__(self):
+        """Returns a String representation for development/testing."""
         s = "VideoContent(title='" + self.title + "', timestamp='" + self.timestamp + "', "\
-            "duration=" + str(self.duration) + ", videourl=" + str(self.video_url()) + ", "\
+            "duration=" + str(self.duration) + ", videourl=" + str(self.video_url('L')) + ", "\
             "imageurl=" + str(self.image_url()) + ")"
         return s.encode('utf-8', 'ignore')
 
-# parses the video JSON into VideoContent
+# 
+def _get(dic, key):
+    """Helper method that returns None if key is not found."""
+    if key in dic:
+        return dic[key]
+    else:
+        return None
+    
+
 def _parse_video(jsonvideo):
+    """Parses the video JSON into a VideoContent object."""
     title = jsonvideo["headline"]
     # TODO: parse into datetime
     timestamp = jsonvideo["broadcastDate"]
@@ -51,8 +92,8 @@ def _parse_video(jsonvideo):
     duration = (jsonvideo["outMilli"] - jsonvideo["inMilli"]) / 1000    
     return VideoContent(title, timestamp, videourls, imageurls, duration);       
 
-# parses the broadcast JSON into VideoContent
 def _parse_broadcast(jsonbroadcast):
+    """Parses the broadcast JSON into a VideoContent object."""
     title = jsonbroadcast["title"]
     # TODO: parse into datetime
     timestamp = jsonbroadcast["broadcastDate"]
@@ -61,16 +102,16 @@ def _parse_broadcast(jsonbroadcast):
     videourls = { "h264l" : "TODO" }
     return VideoContent(title, timestamp, videourls, imageurls);
 
-# parses the image variants JSON into a dict mapping name to url 
 def _parse_image_urls(jsonvariants):
+    """Parses the image variants JSON into a dict mapping variant name to URL.""" 
     variants = {}
     for jsonvariant in jsonvariants:
         for name, url in jsonvariant.iteritems():
             variants[name] = url
     return variants
 
-# parses the video mediadata JSON into a dict mapping name to url
 def _parse_video_urls(jsonvariants):
+    """Parses the video mediadata JSON into a dict mapping variant name to URL."""
     variants = {}
     for jsonvariant in jsonvariants:
         for name, url in jsonvariant.iteritems():
@@ -78,6 +119,11 @@ def _parse_video_urls(jsonvariants):
     return variants
 
 def latest_videos():
+    """Retrieves the latest videos.
+        
+        Returns: 
+            A list of VideoContent items.
+    """
     logger.info("retrieving videos");
     videos = []
     handle = urllib2.urlopen("http://www.tagesschau.de/api/multimedia/video/ondemand100_type-video.json")
@@ -85,10 +131,15 @@ def latest_videos():
     for jsonvideo in response["videos"]:
         video = _parse_video(jsonvideo)
         videos.append(video) 
-    logger.info("found "+str(len(videos))+" videos")
+    logger.info("found " + str(len(videos)) + " videos")
     return videos
 
 def dossiers():
+    """Retrieves the latest dossier videos.
+        
+        Returns: 
+            A list of VideoContent items.
+    """    
     logger.info("retrieving videos");
     videos = []
     handle = urllib2.urlopen("http://www.tagesschau.de/api/multimedia/video/ondemanddossier100.json")
@@ -96,10 +147,15 @@ def dossiers():
     for jsonvideo in response["videos"]:
         video = _parse_video(jsonvideo)
         videos.append(video)
-    logger.info("found "+str(len(videos))+" videos")            
+    logger.info("found " + str(len(videos)) + " videos")            
     return videos
 
 def latest_broadcasts():
+    """Retrieves the latest broadcast videos.
+        
+        Returns: 
+            A list of VideoContent items.
+    """        
     logger.info("retrieving videos");    
     videos = []
     handle = urllib2.urlopen("http://www.tagesschau.de/api/multimedia/sendung/letztesendungen100.json")
@@ -107,7 +163,7 @@ def latest_broadcasts():
     for jsonbroadcast in response["latestBroadcastsPerType"]:
         video = _parse_broadcast(jsonbroadcast)
         videos.append(video)
-    logger.info("found "+str(len(videos))+" videos")              
+    logger.info("found " + str(len(videos)) + " videos")              
     return videos
 
 if __name__ == "__main__":
